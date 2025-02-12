ce187723add29a9124f315d7d7a04d4eb30b2bc3
Aadil Gupta <guptaaadil96@gmail.com>
2025-02-12 17:12:47 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-website-backend-ila6.onrender.com'
+export const url = 'https://food-delivering-website-backend.onrender.com'
