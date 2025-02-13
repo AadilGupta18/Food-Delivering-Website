5407f9dd501d6f9bb9e9af7eaedef9b015b29c0b
Aadil Gupta <guptaaadil96@gmail.com>
2025-02-14 01:03:28 +0530

    Update db.js for hosting purposes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect("mongodb+srv://ishanbeast662:mongodbfood@cluster0.kzhge.mongodb.net/food-del")
+    .connect("mongodb+srv://aadil:Aadil%40123@foodiefrizzy.thctk.mongodb.net/?retryWrites=true&w=majority&appName=FoodieFrizzy")
     .then(() => console.log("DB Connected"));
 };
