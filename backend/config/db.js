1d8fa97b5f72b6668e84dfb61eb8788757582ad3
Aadil Gupta <guptaaadil96@gmail.com>
2025-02-14 01:21:21 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect("mongodb+srv://aadil:Aadil%40123@foodiefrizzy.thctk.mongodb.net/?retryWrites=true&w=majority&appName=FoodieFrizzy")
+    .connect("mongodb+srv://aadil:Aadil%40123@foodiefrizzy.thctk.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsInsecure=true&appName=FoodieFrizzy")
     .then(() => console.log("DB Connected"));
 };
