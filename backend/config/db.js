ef34735d070e05a2d991533b5909ae91de10e87b
Aadil Gupta <guptaaadil96@gmail.com>
2025-02-14 00:45:48 +0530

    Updated db.js for hosting purposes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect("mongodb://localhost:27017/")
+    .connect("mongodb+srv://ishanbeast662:mongodbfood@cluster0.kzhge.mongodb.net/food-del")
     .then(() => console.log("DB Connected"));
 };
